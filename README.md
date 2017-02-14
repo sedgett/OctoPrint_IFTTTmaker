6a444e81df9b88dd711ddaffa361301ce7d15456
Simon Edgett <simon@edget.bc.ca>
2017-02-14 15:32:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# OctoPrint_IFTTTmaker
+
+Connect Octoprint to the IFTTT maker channel.
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
+or manually using this URL:
+
+    https://github.com/sedgett/OctoPrint_IFTTTmaker/archive/master.zip
+
+## Configuration
+
+1. Setup the IFTTT maker channel at https://ifttt.com/maker/ and enter your key in the settings screen.
+2. Create recipies on IFTTT
+3. Select the events to send to IFTTT via settings.
+
---
+++ octoprint_IFTTTmaker/__init__.py
@@ -0,0 +1,88 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+import requests
+from octoprint.events import Events
+
+import os
+
+
+class IFTTTMakerPlugin(octoprint.plugin.StartupPlugin,
+                        octoprint.plugin.TemplatePlugin,
+                        octoprint.plugin.SettingsPlugin,
+                        octoprint.plugin.EventHandlerPlugin):
+
+    def on_settings_save(self, data):
+        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        self.makerkey = self._settings.get(["makerkey"])
+        self.ClientOpen = self._settings.get_boolean(["ClientOpen"])
+        self._logger.info("Saving IFTTT Maker Key: %s" % self.makerkey)
+        
+    def on_after_startup(self):
+        self._logger.info("IFTTT Maker Plugin Active")
+        self.makerkey = self._settings.get(["makerkey"])
+        self._logger.debug("IFTTT Maker Key: %s" % self.makerkey)
+
+    def get_settings_defaults(self):
+        return dict(makerkey='ENTER_YOUR_MAKER_KEY',
+                    events=dict(
+                        PrintStarted=False,
+                        PrintFailed=False,
+                        PrintCancelled=False,
+                        PrintDone=False,
+                        MovieDone=False,
+                        ClientOpened=False)
+                    )
+                
+            
+    def get_template_configs(self):
+        return [ dict(type="settings", name="IFTTTmaker", custom_bindings=False) ]
+
+    def get_settings_restricted_paths(self):
+        # only used in OctoPrint versions > 1.2.16
+        return dict(admin=[["makerkey"]])
+
+######
+
+    def on_event(self, event, payload):
+        events = self._settings.get(['events'], merged=True)
+        
+        if event in events:
+            v1 = v2 = v3 = ""
+            if 'file' in payload:
+                v1 = os.path.basename(payload["file"])
+            if 'time' in payload:
+                v2 = payload["time"]
+            if 'remoteAddress' in payload:
+                v3 = payload["remoteAddress"]
+            self._send_ifttt("op-"+event, v1, v2)
+        else:
+            self._logger.debug("Event skipped: %s" % event)
+          
+        
+#         == Events.PRINT_DONE:
+#            file = os.path.basename(payload["file"])
+#            elapsed_time_in_seconds = payload["time"]
+#            import datetime
+#            import octoprint.util    
+#            elapsed_time = octoprint.util.get_formatted_timedelta(datetime.timedelta(seconds=elapsed_time_in_seconds))
+#            self._send_ifttt("op-PrintDone", file, elapsed_time)
+
+
+
+    def _send_ifttt(self, trigger, value1=None, value2=None, value3=None):
+        import requests
+        payload = "{ 'value1' : value1, 'value2' : value2, 'value3' : value3}"
+        url = "https://maker.ifttt.com/trigger/" + trigger + "/with/key/" + self.makerkey
+        res = requests.post(url, data=payload)
+        self._logger.debug("URL: %s" % url)
+        self._logger.info("Trigger: %s Response:: %s" % (trigger,  res.text))
+        
+                                                                                            
+######
+                        
+__plugin_name__ = "IFTTT Maker"
+__plugin_implementation__ = IFTTTMakerPlugin()
+
+
