3b1a60e36ab9a2ce4959bd8ca6f21e1458e8450a
Simon Edgett <simon@edget.bc.ca>
2017-02-15 01:24:25 -0500

    changed version #

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_IFTTTmaker"
 plugin_name = "OctoPrint_IFTTTmaker"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.0"
+plugin_version = "1.0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
